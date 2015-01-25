e25a4355ec3107da4a0000f226fa94569707daf8
Offir Talmor <offir.talmor@jivesoftware.com>
2015-01-25 13:55:24 +0200

    First commit.. adding project

---
+++ interview/interview/JVIAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  interview
+//
+//  Created by Offir Talmor on 1/25/15.
+//  Copyright (c) 2015 Jive Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JVIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ interview/interview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  interview
+//
+//  Created by Offir Talmor on 1/25/15.
+//  Copyright (c) 2015 Jive Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JVIAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JVIAppDelegate class]));
+    }
+}
